7f54a73724ad31736e3d71807741dea59fe01c0c
Danny <oo482338@gmail.com>
2026-05-29 21:30:39 +0800

    Initialize images.js with placeholder image assets

---
+++ images.js
@@ -0,0 +1,4 @@
+// Image assets
+export const LOGO_IMG = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgZmlsbD0iI2RkZDAyYyIvPjwvc3ZnPg==";
+
+export const KIMCHI_IMG = "";
